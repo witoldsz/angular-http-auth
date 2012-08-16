8b941f935769fbd7475af8083b5053f249a69f84
Witold Szczerba <pljosh.mail@gmail.com>
2012-08-16 02:04:23 +0200

    Removed unnecessary mock content.

--- app/content-mocks.js
+++ app/content-mocks.js
@@ -4,17 +4,17 @@ angular.module('content-mocks',['ngMockE2E'])
     var authorized = false;
     $httpBackend.whenPOST('auth/login').respond(function(method, url, data) {
       authorized = true;
-      return [200,''];
+      return [200];
     });
     $httpBackend.whenPOST('auth/logout').respond(function(method, url, data) {
       authorized = false;
-      return [200,''];
+      return [200];
     });
     
     
     $httpBackend.whenGET('data/public').respond("Public content");
     $httpBackend.whenGET('data/protected').respond(function() {
-      return authorized ? [200,'Protected content available'] : [401,'Auth required'];
+      return authorized ? [200,'Protected content available'] : [401];
     });
 
     //otherwise
