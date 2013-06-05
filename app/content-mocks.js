0a5acb422b93ba39743546c67231a6b015af6f8a
Stu Salsbury <stu.salsbury@gmail.com>
2013-06-05 10:52:04 -0700

    demonstrate support for 403/forbidden and cancel -- this is a bit sloppy but may be of use

--- app/content-mocks.js
+++ app/content-mocks.js
@@ -4,22 +4,35 @@
 angular.module('content-mocks',['ngMockE2E'])
   .run(function($httpBackend) {
 
-    var authorized = false;
+    var user = null;
     $httpBackend.whenPOST('auth/login').respond(function(method, url, data) {
-      authorized = true;
+      if (angular.isString(data)) {
+        try {
+          user = angular.fromJson(data).username;
+        }
+        catch (err) {
+          return [500];
+        }
+      }
+      else {
+        user = null;
+      }
       return [200];
     });
     $httpBackend.whenPOST('auth/logout').respond(function(method, url, data) {
-      authorized = false;
+      user = null;
       return [200];
     });
-    
-    
+
+
     $httpBackend.whenPOST('data/public').respond(function(method, url, data) {
       return [200,'I have received and processed your data [' + data + '].'];
     });
     $httpBackend.whenPOST('data/protected').respond(function(method, url, data) {
-      return authorized ? [200,'This is confidential [' + data + '].'] : [401];
+      return user ? [200,'This is confidential [' + data + '].'] : [401];
+    });
+    $httpBackend.whenPOST('data/adminOnly').respond(function(method, url, data) {
+      return user === 'admin' ? [200,'This is admin-only [' + data + '].'] : user ? [403, 'admin'] : [401];
     });
 
     //otherwise
