9d223ca274d566715a45230b5bb1f9d4d27428a4
Witold Szczerba <pljosh.mail@gmail.com>
2013-10-31 11:21:36 +0100

    Revert "minor cleanup: iterating over array using `for x in y`"
    This reverts commit 301dfe9e2007a686fdc9bbfa3ca8c5f6f50302ec.
    See: https://github.com/witoldsz/angular-http-auth/pull/48

--- src/http-auth-interceptor.js
+++ src/http-auth-interceptor.js
@@ -107,7 +107,7 @@
        */
       rejectAll: function(reason) {
         if (reason) {
-          for (var i in buffer) {
+          for (var i = 0; i < buffer.length; ++i) {
             buffer[i].deferred.reject(reason);
           }
         }
@@ -118,7 +118,7 @@
        * Retries all the buffered requests clears the buffer.
        */
       retryAll: function(updater) {
-        for (var i in buffer) {
+        for (var i = 0; i < buffer.length; ++i) {
           retryHttpRequest(updater(buffer[i].config), buffer[i].deferred);
         }
         buffer = [];
